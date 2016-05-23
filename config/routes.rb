3a300a7adda29ae5905171c1c16543901e813e7b
Angel Vanegas <angelman@gmail.com>
2016-05-23 15:50:28 -0400

    create product seed

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   post 'welcome' => 'messages#create'
 
+  get 'products' => 'products#index'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160523194449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string   "collection"
+    t.string   "name"
+    t.text     "description"
+    t.text     "skintype"
+    t.text     "benefits"
+    t.text     "directions"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
