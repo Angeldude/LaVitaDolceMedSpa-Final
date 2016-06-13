06ed0e34ad94ad6652f3446dc1653033ea8b797c
Jorge Naranjo <j.naranjojr@gmail.com>
2016-06-13 16:06:20 -0400

    Disable the popup

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,6 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( bootstrap.css plugins.css homePopup.css style-pink.css responsive.css animate.css jquery.min.js jquery.isotope.min.js sorting.js homePopupLoad.js homePopup.js plugins.js current.js wow.min.js )
+
+# Add 'homePopupLoad.js homePopup.js homePopup.css' for the popup window to render
+Rails.application.config.assets.precompile += %w( bootstrap.css plugins.css  style-pink.css responsive.css animate.css jquery.min.js jquery.isotope.min.js sorting.js plugins.js current.js wow.min.js )
